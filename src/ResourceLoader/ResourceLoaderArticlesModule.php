ab42315aef73190b1e87c169cf2bef701a4c2fa1
Alex Winkler <fo.nttax@yahoo.de>
2025-06-13 13:35:59 +0200

    Make the extension compatible with MW 1.43

--- src/ResourceLoader/ResourceLoaderArticlesModule.php
+++ src/ResourceLoader/ResourceLoaderArticlesModule.php
@@ -10,25 +10,25 @@ use CSSJanus;
 use Less_Parser;
 use MediaWiki\MainConfigNames;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\ResourceLoader\Context;
+use MediaWiki\ResourceLoader\ResourceLoader;
+use MediaWiki\ResourceLoader\WikiModule;
 use MemoizedCallable;
 use Peast\Peast;
 use Peast\Syntax\Exception as PeastSyntaxException;
-use ResourceLoader;
-use ResourceLoaderContext;
-use ResourceLoaderWikiModule;
 use ScssPhp\ScssPhp\Compiler as SCSSCompiler;
 
-class ResourceLoaderArticlesModule extends ResourceLoaderWikiModule {
+class ResourceLoaderArticlesModule extends WikiModule {
 
 	private const USERJSPARSE_CACHE_VERSION = 3;
 
 	/**
 	 * Get list of pages used by this module
 	 *
-	 * @param ResourceLoaderContext $context
+	 * @param Context $context
 	 * @return array List of pages
 	 */
-	protected function getPages( ResourceLoaderContext $context ) {
+	protected function getPages( Context $context ) {
 		$request = $context->getRequest();
 		$articles = $request->getVal( 'articles' );
 		$articles = explode( '|', $articles ?? '' );
@@ -100,10 +100,10 @@ class ResourceLoaderArticlesModule extends ResourceLoaderWikiModule {
 	}
 
 	/**
-	 * @param ResourceLoaderContext $context
+	 * @param Context $context
 	 * @return array
 	 */
-	public function getStyles( ResourceLoaderContext $context ) {
+	public function getStyles( Context $context ) {
 		$less = '';
 		$scss = '';
 		foreach ( $this->getPages( $context ) as $titleText => $options ) {
